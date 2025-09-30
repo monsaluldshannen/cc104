064f15d5ecad98146ee5adb8c77bb570a6cafbb3
kimp34435@gmail.com <deathsent28@gmail.com>
2025-09-30 13:10:38 +0800

    add my list

---
+++ m23stringops.py
@@ -0,0 +1,8 @@
+str = "hello world"
+print("First char:", str[0])
+
+for string in str:
+     print(end=" ")
+s2 = str + "!!!"
+print("\nConcatenated:", s2)
+print("Contains 'world'?", "world" in str)
